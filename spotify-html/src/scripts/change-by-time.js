c860d62fac8db903dbfd352106945225529578dd
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2025-01-29 12:43:52 -0300

    Aula 03

---
+++ spotify-html/src/scripts/change-by-time.js
@@ -0,0 +1,18 @@
+const date = new Date()
+let hour = date.getHours()
+let saudacao
+
+if(hour < 12) {
+    saudacao = "Bom dia"
+} 
+
+if(hour >= 12 && hour < 19) {
+    saudacao = "Boa tarde"
+}
+
+if(hour >= 19) {
+    saudacao = "Boa noite"
+}
+
+const saudacaoPlace = document.querySelector(".saudacao-change-by-time")
+saudacaoPlace.innerHTML = saudacao
